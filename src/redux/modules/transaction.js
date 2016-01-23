1055077df47932f2c4652f288de941a856d98989
Esteban Ordano <eordano@gmail.com>
2016-01-23 01:44:43 -0800

    feat(tx): Skeleton of a tx creation

---
+++ src/redux/modules/transaction.js
@@ -0,0 +1,14 @@
+import bitcore from 'bitcore-lib'
+
+export const NEW_TRANSACTION = 'NEW_TRANSACTION'
+
+export const ADD_INPUT = 'ADD_INPUT'
+export const ADD_SIGNATURE = 'ADD_SIGNATURE'
+export const ADD_OUTPUT = 'ADD_OUTPUT'
+
+export const REMOVE_INPUT = 'REMOVE_INPUT'
+export const REMOVE_OUTPUT = 'REMOVE_OUTPUT'
+export const REMOVE_SIGNATURE = 'REMOVE_SIGNATURE'
+
+export const SET_VERSION = 'SET_VERSION'
+export const SET_LOCKTIME = 'SET_LOCKTIME'
